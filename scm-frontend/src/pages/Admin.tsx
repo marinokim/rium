bed8fc1a9c387f41a253a115ae6938e4b415b0c7
marinokim <twovol@gmail.com>
2025-12-14 10:31:16 +0900

    fix(frontend): Remove unused React import to fix build error

--- scm-frontend/src/pages/Admin.tsx
+++ scm-frontend/src/pages/Admin.tsx
@@ -1,5 +1,5 @@
 
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import api from '../api/axios';
 
